818ea4b6b884a30db1a04a51fdf054f841f0ad51
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-25 21:29:28 -0400

    changed handlebars files to incorporate a login form. Respective controller routes and javascript added to post and validate user login.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,4 +33,16 @@ router.get('/', async (req, res) => {
     };
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 module.exports = router;
