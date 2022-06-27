e7de7b62cb7c1c78adbccb77330591c7a407e141
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-26 21:20:13 -0400

    multiple files updated to handle editing and deleting posts. edit-post.handlebars created to present users with a view that allows them to edit their post in a view. javascript documents have been added to handle the logic of sending put and delete requests and redirecting back to the dashboard. post-routes.js has been updated with put and delete routes that handle updating and deleting posts respectively. minor tweaks to some of the views for styling purposes. Testing indicates that all features are working properly.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,9 +6,9 @@ router.post('/', withAuth, async (req, res) => {
     
     try {
         const createPost = await Post.create({
-        title: req.body.title,
-        post_content: req.body.post_content,
-        user_id: req.session.user_id
+            title: req.body.title,
+            post_content: req.body.post_content,
+            user_id: req.session.user_id
         })
 
         res.status(200).json({ 
@@ -17,6 +17,65 @@ router.post('/', withAuth, async (req, res) => {
         });
     } catch (err) {
         console.log(err);
+        res.status(500).json({ 
+            message: "Internal server error",
+            error: err 
+        });
+    };
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatePost = await Post.update(
+        {
+            title: req.body.title,
+            post_content: req.body.post_content
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!updatePost) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        res.status(200).json({ 
+            message: "Successfully updated post",
+            data: updatePost
+        });
+    } catch (err) {
+     
+        res.status(500).json({ 
+            message: "Internal server error",
+            error: err 
+        });
+
+    }
+});
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletePost = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        
+        if (!deletePost) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        res.status(200).json({ 
+            message: "Successfully deleted post",
+            data: deletePost
+        });
+
+    } catch (err) {
+
         res.status(500).json({ 
             message: "Internal server error",
             error: err 
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -28,7 +28,7 @@ router.get('/', withAuth, async (req, res) => {
 
         const posts = dbPostData.map(post => post.get({ plain: true }));
         console.log(posts);
-        res.render('dashboard', { posts, loggedIn: true });
+        res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
         
     } catch (err) {
 
@@ -36,6 +36,52 @@ router.get('/', withAuth, async (req, res) => {
         res.status(500).json(err);
 
     };
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+
+    try {
+        const dbPostData = await Post.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: [
+                'id',
+                'title',
+                'post_content',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+        res.render('edit-post', { post, loggedIn: req.session.loggedIn });
+        
+    } catch (err) {
+
+        console.log(err);
+        res.status(500).json(err);
+
+    };
+
 });
 
 module.exports = router;
